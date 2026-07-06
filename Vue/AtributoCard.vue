92581e1248201eaeee9134052a1a84f5ec0cbbdf
João Pereira Neto <joaopereiranet33@gmail.com>
2026-07-05 21:33:38 -0300

    Adição dos componentes AtributoCard e FichaPersonagem

---
+++ Vue/AtributoCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+defineProps({
+        name: String,
+        valor: Number
+})
+
+const emit = defineEmits(['aumentar', 'diminuir'])
+
+</script>
+
+<template>
+  <div>
+    <h2>{{ name }}: {{ valor }}</h2>
+    <button @click="emit('diminuir')">-</button>
+    <button @click="emit('aumentar')">+</button>
+  </div>
+</template>
---
+++ Vue/FichaPersonagem.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+import AtributoCard from './AtributoCard.vue';
+
+const atributos = ref([
+  { nome: 'Agilidade', valor: 1 },
+  { nome: 'Força', valor: 1 },
+  { nome: 'Intelecto', valor: 1 },
+  { nome: 'Presença', valor: 1 },
+  { nome: 'Vigor', valor: 1 },
+])
+
+function aumentar(atributo) {
+  if (atributo.valor < 5) {
+    atributo.valor++;
+  }
+}
+
+function diminuir(atributo) {
+  if (atributo.valor > 0) {
+    atributo.valor--;
+  }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Ficha do Personagem</h1>
+        <AtributoCard
+        v-for="atributo in atributos"
+        :key="atributo.nome"
+        :name="atributo.nome"
+        :valor="atributo.valor"
+        @aumentar="aumentar(atributo)"
+        @diminuir="diminuir(atributo)"
+        />
+    </div>
+
+</template>
